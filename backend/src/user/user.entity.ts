66bbb14a38737938a4d32e67e983623c1cf2f38a
Moeed Rafi <moeedrafi95@gmail.com>
2026-02-23 23:21:15 +0500

    feat: user module

---
+++ backend/src/user/user.entity.ts
@@ -0,0 +1,55 @@
+import {
+  Entity,
+  Column,
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  password: string;
+
+  @Column({ nullable: true, select: true })
+  refreshToken?: string;
+
+  @Column({ nullable: true, select: true })
+  resetToken?: string;
+
+  @Column({ type: 'timestamp', nullable: true })
+  resetTokenExpiry?: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted user with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed user with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('UPdated user with id', this.id);
+  }
+}
