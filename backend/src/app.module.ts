a7cd8f2acfaa1b5e7db2c1ff7db2e4c5688e2ef1
Moeed Rafi <moeedrafi95@gmail.com>
2026-03-05 22:03:47 +0500

    setup: postgres + mailer service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from 'src/app.service';
 import { UserModule } from 'src/user/user.module';
@@ -9,6 +9,7 @@ import { DeckModule } from 'src/deck/deck.module';
 import { AppController } from 'src/app.controller';
 import { AuthGuard } from 'src/common/guards/auth.guard';
 import { FlashcardModule } from 'src/flashcard/flashcard.module';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
@@ -16,11 +17,30 @@ import { FlashcardModule } from 'src/flashcard/flashcard.module';
       envFilePath: '.env',
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        ssl: true,
+        synchronize: true,
+        extra: { max: 5 },
+      }),
+    }),
+    MailerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        transport: {
+          port: 587,
+          secure: false,
+          host: config.get<string>('HOST'),
+          auth: {
+            user: config.get<string>('SMTP_USER'),
+            pass: config.get<string>('SMTP_PASS'),
+          },
+        },
+      }),
     }),
     UserModule,
     AuthModule,
